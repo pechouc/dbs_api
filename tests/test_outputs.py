30b2d595598475ea0c5b96f37743d8cf4839650c
Paul-Emmanuel Chouc <pechouc@gmail.com>
2022-07-03 19:59:37 +0200

    Finalising the test workflow

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -93,8 +93,154 @@ def test_US_outputs():
             assert np.abs(intermediary_df_2[column] - intermediary_df_2_saved[column]).max() < 0.0000001
 
 def test_global_restricted_outputs():
-    pass
+    available_years = [2016, 2017]
+
+    for year in available_years:
+        analyses_provider = GlobalAnalysesProvider(
+            year=year,
+            aamne_domestic_sales_perc=True,
+            breakdown_threshold=60,
+            US_merchandise_exports_source='Comtrade',
+            US_services_exports_source='BaTIS',
+            non_US_merchandise_exports_source='Comtrade',
+            non_US_services_exports_source='BaTIS',
+            winsorize_export_percs=True,
+            non_US_winsorizing_threshold=0.5,
+            US_winsorizing_threshold=0.5,
+            service_flows_to_exclude=[],
+            macro_indicator='CONS',
+            load_data_online=False
+        )
+
+        sales_mapping = analyses_provider.sales_mapping.copy()
+        sales_mapping_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'sales_mapping_global_restricted_{year}.csv')
+        )
+
+        intermediary_df_1 = analyses_provider.get_intermediary_dataframe_1(
+            include_macro_indicator=True,
+            exclude_US_from_parents=False
+        ).reset_index(drop=True)
+        intermediary_df_1_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'intermediary_dataframe_1_global_restricted_{year}.csv')
+        )
+
+        intermediary_df_2 = analyses_provider.get_intermediary_dataframe_2(
+            include_macro_indicator=True,
+            exclude_US_from_parents=False
+        ).reset_index(drop=True)
+        intermediary_df_2_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'intermediary_dataframe_2_global_restricted_{year}.csv')
+        )
+
+        oecd = analyses_provider.oecd.copy()
+        oecd = oecd.reset_index(drop=True)
+        oecd_saved = pd.read_csv(os.path.join(path_to_outputs, f'oecd_{year}_restricted.csv'))
+
+        for idx in [0, 1]:
+            assert sales_mapping.shape[idx] == sales_mapping_saved.shape[idx]
+            assert intermediary_df_1.shape[idx] == intermediary_df_1_saved.shape[idx]
+            assert intermediary_df_2.shape[idx] == intermediary_df_2_saved.shape[idx]
+            assert oecd.shape[idx] == oecd_saved.shape[idx]
+
+        for column in ['UNRELATED_PARTY_REVENUES', 'RELATED_PARTY_REVENUES', 'TOTAL_REVENUES']:
+            assert np.abs(
+                (
+                    (sales_mapping[column] - sales_mapping_saved[column])
+                    / sales_mapping_saved[column].map(lambda x: 1 if x == 0 else x)
+                    * 100
+                )
+            ).max() < 0.00001
+            assert np.abs(
+                (
+                    (oecd[column] - oecd_saved[column])
+                    / oecd_saved[column].map(lambda x: 1 if x == 0 else x)
+                    * 100
+                )
+            ).max() < 0.00001
+
+        for column in [
+            'SHARE_OF_UNRELATED_PARTY_REVENUES',
+            'SHARE_OF_RELATED_PARTY_REVENUES',
+            'SHARE_OF_TOTAL_REVENUES',
+            f'SHARE_OF_CONS_{year}'
+        ]:
+            assert np.abs(intermediary_df_1[column] - intermediary_df_1_saved[column]).max() < 0.0000001
+            assert np.abs(intermediary_df_2[column] - intermediary_df_2_saved[column]).max() < 0.0000001
 
 
 def test_global_unrestricted_outputs():
-    pass
+    available_years = [2016, 2017]
+
+    for year in available_years:
+        analyses_provider = GlobalAnalysesProvider(
+            year=year,
+            aamne_domestic_sales_perc=True,
+            breakdown_threshold=0,
+            US_merchandise_exports_source='Comtrade',
+            US_services_exports_source='BaTIS',
+            non_US_merchandise_exports_source='Comtrade',
+            non_US_services_exports_source='BaTIS',
+            winsorize_export_percs=True,
+            non_US_winsorizing_threshold=0.5,
+            US_winsorizing_threshold=0.5,
+            service_flows_to_exclude=[],
+            macro_indicator='CONS',
+            load_data_online=False
+        )
+
+        sales_mapping = analyses_provider.sales_mapping.copy()
+        sales_mapping_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'sales_mapping_global_unrestricted_{year}.csv')
+        )
+
+        intermediary_df_1 = analyses_provider.get_intermediary_dataframe_1(
+            include_macro_indicator=True,
+            exclude_US_from_parents=False
+        ).reset_index(drop=True)
+        intermediary_df_1_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'intermediary_dataframe_1_global_unrestricted_{year}.csv')
+        )
+
+        intermediary_df_2 = analyses_provider.get_intermediary_dataframe_2(
+            include_macro_indicator=True,
+            exclude_US_from_parents=False
+        ).reset_index(drop=True)
+        intermediary_df_2_saved = pd.read_csv(
+            os.path.join(path_to_outputs, f'intermediary_dataframe_2_global_unrestricted_{year}.csv')
+        )
+
+        oecd = analyses_provider.oecd.copy()
+        oecd = oecd.reset_index(drop=True)
+        oecd_saved = pd.read_csv(os.path.join(path_to_outputs, f'oecd_{year}_unrestricted.csv'))
+
+        for idx in [0, 1]:
+            assert sales_mapping.shape[idx] == sales_mapping_saved.shape[idx]
+            assert intermediary_df_1.shape[idx] == intermediary_df_1_saved.shape[idx]
+            assert intermediary_df_2.shape[idx] == intermediary_df_2_saved.shape[idx]
+            assert oecd.shape[idx] == oecd_saved.shape[idx]
+
+        for column in ['UNRELATED_PARTY_REVENUES', 'RELATED_PARTY_REVENUES', 'TOTAL_REVENUES']:
+            assert np.abs(
+                (
+                    (sales_mapping[column] - sales_mapping_saved[column])
+                    / sales_mapping_saved[column].map(lambda x: 1 if x == 0 else x)
+                    * 100
+                )
+            ).max() < 0.00001
+            assert np.abs(
+                (
+                    (oecd[column] - oecd_saved[column])
+                    / oecd_saved[column].map(lambda x: 1 if x == 0 else x)
+                    * 100
+                )
+            ).max() < 0.00001
+
+        for column in [
+            'SHARE_OF_UNRELATED_PARTY_REVENUES',
+            'SHARE_OF_RELATED_PARTY_REVENUES',
+            'SHARE_OF_TOTAL_REVENUES',
+            f'SHARE_OF_CONS_{year}'
+        ]:
+            assert np.abs(intermediary_df_1[column] - intermediary_df_1_saved[column]).max() < 0.0000001
+            assert np.abs(intermediary_df_2[column] - intermediary_df_2_saved[column]).max() < 0.0000001
